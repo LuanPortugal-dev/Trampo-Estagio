e833254341753d5e6088408e7e779db95e03e6b9
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2021-03-01 20:57:09 -0300

    Studying Python in DataScience Academy

---
+++ DataScience/Study/Módulo2/exercicios.py
@@ -0,0 +1,57 @@
+# ------------------- Exercícios Cap02 -------------------
+
+# Exercício 1 - Imprima na tela os números de 1 a 10. Use uma lista para armazenar os números.
+#for i in range(1,11):
+    #print(i)
+
+
+# Exercício 2 - Crie uma lista de 5 objetos e imprima na tela
+#objetos = ['Python', 'R', 'Julia', 'Pandas', 'DataFrame']
+#print(objetos)
+
+
+# Exercício 3 - Crie duas strings e concatene as duas em uma terceira string
+#luan = 'Portugal'
+#silva = 'da silva'
+#print(luan + ' ' + silva)
+
+
+# Exercício 4 - Crie uma tupla com os seguintes elementos: 1, 2, 2, 3, 4, 4, 4, 5 e depois utilize a função count do
+# objeto tupla para verificar quantas vezes o número 4 aparece na tupla
+#tupla = (range(1,6))
+#print(tupla.count(4))
+
+
+# Exercício 5 - Crie um dicionário vazio e imprima na tela
+#dic = {}
+#print(dic)
+
+
+# Exercício 6 - Crie um dicionário com 3 chaves e 3 valores e imprima na tela
+#dic = {"Luan": 20, "Ester": 21, "Mãe": 33}
+#print(dic)
+
+
+# Exercício 7 - Adicione mais um elemento ao dicionário criado no exercício anterior e imprima na tela
+#dic = {"Luan": 20, "Ester": 21, "Mãe": 33}
+#dic["BRA"] = (35)
+#print(dic)
+
+
+# Exercício 8 - Crie um dicionário com 3 chaves e 3 valores. Um dos valores deve ser uma lista de 2 elementos numéricos.
+# Imprima o dicionário na tela.
+#dic = {"Luan": 20, "Ester": [21, 20], "Mãe": 33}
+#print(dic)
+
+
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string,
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e
+# o quarto elemento um valor do tipo float.
+# Imprima a lista na tela.
+#lista = ['Luan', (20, 21), {"Luan": 20, "Ester": 21}, 5.5]
+#print(lista)
+
+
+# Exercício 10 - Considere a string abaixo. Imprima na tela apenas os caracteres da posição 1 a 18.
+#frase = 'Cientista de Dados é o profissional mais sexy do século XXI'
+#print(frase[0:18])
