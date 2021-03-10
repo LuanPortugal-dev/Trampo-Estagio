7a74c40acbdd6569568a1b2ef1d3bec48fa8926e
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2021-03-09 22:51:20 -0300

    Finishi project put filter using Selenium

--- Selenium-huggy/HuggyColocarFiltro.py
+++ Selenium-huggy/HuggyColocarFiltro.py
@@ -16,10 +16,11 @@ class ChromeAutenticao:
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
         self.driver = webdriver.Chrome(         # OBTEM O DIRETÓRIO E USA O CHROMEDRIVER
-            executable_path='C:/Users/luan.portugal/Desktop/HuggySelenium/chromedriver.exe')
-        
+            executable_path='D:/TRABALHO/Selenium-huggy/chromedriver.exe')
+                
+# 'C:/Users/luan.portugal/Desktop/HuggySelenium/chromedriver.exe'
 
-    def ColocarFlow(self): # FUNÇÃO PARA INCIAR COLOCAR FLOW
+    def Colocar_Flow(self): # FUNÇÃO PARA INCIAR A TROCA DO FLOW
         self.driver.get('https://www.huggy.app/?return=https%3A%2F%2Flegacy.huggy.app%2Fpanel%2F')   # ACESSA O LINK NO QUAL SE FAZ O SELENIUM
         self.driver.maximize_window()    # COLOCAR A TELA CHEIA DE ACORDO COM A RESOLUÇÃO DA TELA 
         time.sleep(2)  # ESPERA 2 SEGUNDOS ANTES DE INICIAR
@@ -32,45 +33,28 @@ class ChromeAutenticao:
         self.driver.find_element(By.CSS_SELECTOR, ".Button_button_3qErG:nth-child(3)").click()    # CLICAR NO INPUT DE EMAIL
         time.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA
         self.driver.find_element(By.ID, "input_17").click()     # CLILCAR NO INPUT SENHA
-        self.driver.find_element(By.ID, "input_17").send_keys("dawdawdawdawdadawd") # COLOCAR A SENHA NO INPUT SENHA
+        self.driver.find_element(By.ID, "input_17").send_keys("aaaaaaadddddaaaaa") # COLOCAR A SENHA NO INPUT SENHA
         time.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA
         self.driver.find_element(By.CSS_SELECTOR, ".Button_block_2cQNN").click() # CLICAR NO BOTÃO DE LOGIN E FAZER LOGIN
 
 
 ################################################# SESSÃO DENTRO DO HUGGY, ACESSANDO ATÉ A TROCA DO FLOW ##################################################
 
-        #self.driver.find_element(By.CSS_SELECTOR, ".company_3lFvn:nth-child(2)").click()     # CLILCAR NA CONTA DA BRA
-        #timme.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR EM CONFIGURAÇÕES
-        #self.driver.find_element(By.CSS_SELECTOR, ".pz_settings").click()      # CLICAR EM CONFIGURAÇÕES
-
-        #element = self.driver.find_element(By.CSS_SELECTOR, "#menubar-config > .module_3KJrf")     # EFEITOS DA PÁGINA
-        #actions = ActionChains(self.driver)    # EFEITOS DA PÁGINA
-        #actions.move_to_element(element).perform() # EFEITOS DA PÁGINA
-        #element = self.driver.find_element(By.LINK_TEXT, "Canais") # EFEITOS DA PÁGINA
-        #actions = ActionChains(self.driver) # EFEITOS DA PÁGINA
-        #actions.move_to_element(element).perform() # EFEITOS DA PÁGINA
-
-        #timme.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR EM CANAIS
-        #self.driver.find_element(By.LINK_TEXT, "Canais").click()     # CLILCAR EM CANAIS
-        
-        #element = self.driver.find_element(By.CSS_SELECTOR, "body") # EFEITOS DA PÁGINA
-        #actions = ActionChains(self.driver) # EFEITOS DA PÁGINA
-        #actions.move_to_element(element, 0, 0).perform() # EFEITOS DA PÁGINA
-
-        #timme.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA
-        #self.driver.find_element(By.CSS_SELECTOR, ".col-lg-6:nth-child(7) .channel_1WciL").click()   # CLICAR EM WHATS BUSINESS API
-        #self.driver.switch_to.frame(1) # RENDERIZA 1 FRAME PARA A EXECUÇÃO
-        #timme.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA
-        #self.driver.find_element(By.CSS_SELECTOR, ".htable-line").click()    # CLICAR EM WHATS VERIFICADO
-        #timme.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA
-        #self.driver.find_element(By.CSS_SELECTOR, ".form-item:nth-child(9) .ms-choice > span").click()    # CLICAR NO SELECIONADOR DE FLOW DE SAÍDA
-        #timme.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA
-        #self.driver.find_element(By.CSS_SELECTOR, ".selected:nth-child(1) span").click()    # CLICAR EM NENHUM FLOW DEFINIDO
-        #timme.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA 
-        #self.driver.find_element(By.CSS_SELECTOR, ".hgs-right > .btn").click()    # CLILCAR EM SALVAR
-        #self.driver.close()        # FECHA O NAVEGADOR
+        time.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO INPUT A SENHA
+        self.driver.get('https://www.huggy.app/panel/config/channels/whatsapp-business-api')
+        time.sleep(2) # ESPERA 2 SEGUNDOS ANTES DE CLILCAR EM WHATS VERIFICADO
+        self.driver.switch_to.frame(1)
+        self.driver.find_element(By.CLASS_NAME, "htable-column-label").click()    # CLICAR EM WHATS VERIFICADO
+        time.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO SELECIONE FLOW
+        self.driver.find_element(By.CSS_SELECTOR, ".form-item:nth-child(9) .ms-choice > span").click()    # CLICAR NO SELECIONADOR DE FLOW DE SAÍDA
+        time.sleep(1) # ESPERA 1 SEGUNDOS ANTES DE CLILCAR NO FLOW DE SAIDA
+        self.driver.find_element(By.CSS_SELECTOR, ".form-item:nth-child(9) li:nth-child(22) > label").click()    # CLICAR NO FLOW: saida_JV
+        time.sleep(2) # ESPERA 2 SEGUNDOS ANTES DE CLILCAR EM SALVAR
+        self.driver.find_element(By.CSS_SELECTOR, ".hgs-right > .btn").click()    # CLILCAR EM SALVAR
+        time.sleep(3) # ESPERA 3 SEGUNDOS ANTES DE FECHAR O NAVEGADOR
+        self.driver.close()        # FECHA O NAVEGADOR
 # FINISH
-        
+
 
 bot = ChromeAutenticao() # INSTANCIAR A CLASSE
-bot.ColocarFlow() # INSTANCIAR A FUNÇÃO DENTRO DA CLASSE
+bot.Colocar_Flow() # INSTANCIAR A FUNÇÃO DENTRO DA CLASSE
